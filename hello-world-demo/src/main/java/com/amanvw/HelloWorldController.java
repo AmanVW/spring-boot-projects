5386e84c462779569e4ccc1eb35f0870e7e1b84c
AmanVW <amanwalke97@gmail.com>
2025-05-11 23:20:59 +0530

    hello workd

---
+++ hello-world-demo/src/main/java/com/amanvw/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.amanvw;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
